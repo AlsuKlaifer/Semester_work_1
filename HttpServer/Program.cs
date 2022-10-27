2bb16fafe27e39f42d5fff5d8d434966c01799ac
Алсу Фаизова <silicium.xxx@gmail.com>
2022-10-27 09:10:28 +0300

    Server
    Набросок сайта и реализация базового функционала

---
+++ HttpServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Server;
+
+bool appIsRunning = true;
+
+var settings = new ServerSettings();
+settings.Serialize();
+var settingsDes = ServerSettings.Deserialize();
+
+var server = new HttpServer();
+
+using (server)
+{
+    server.Start();
+
+    while (appIsRunning)
+    {
+        Handler(Console.ReadLine()?.ToLower(), server);
+    }
+}
+
+static void Handler(string command, HttpServer server)
+{
+    switch (command)
+    {
+        case "start":
+            server.Start();
+            break;
+
+        case "restart":
+            server.Stop();
+            server.Start();
+            break;
+
+        case "stop":
+            server.Stop();
+            break;
+
+        case "status":
+            Console.WriteLine(server.Status);
+            break;
+    }
+}
+
+Console.Read();
---
+++ HttpServer/ServerSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Server
+{
+    public class ServerSettings
+    {
+        public int Port { get; set; } = 7700;
+        public string Path { get; set; } = @"./FashionSite/";
+
+        public ServerSettings() {}
+        public ServerSettings(int port, string path)
+        {
+            Port = port;
+            Path = path;
+        }
+
+        public void Serialize()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(this, options);
+
+            using (StreamWriter streamWriter = new StreamWriter(@"Settings.JSON", false))
+            {
+                streamWriter.WriteLine(jsonString);
+            }
+        }
+        public static ServerSettings Deserialize()
+        {
+            using (FileStream fs = new FileStream(@"Settings.JSON", FileMode.OpenOrCreate))
+            {
+                return JsonSerializer.Deserialize<ServerSettings>(fs);
+            }
+        }
+    }
+}
